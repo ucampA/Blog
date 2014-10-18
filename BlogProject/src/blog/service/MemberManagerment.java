155526051ab3ed0a8f6fecbae41195be6bd8732f
hyo <hyo@Seob>
2014-10-18 18:15:28 +0900

    블로그,멤버 서비스 완성

--- BlogProject/src/blog/service/MemberManagerment.java
+++ BlogProject/src/blog/service/MemberManagerment.java
@@ -1,5 +1,77 @@
 package blog.service;
 
+import java.sql.SQLException;
+
+import net.sf.json.JSONArray;
+import blog.exception.RecordNotFoundException;
+import blog.model.dao.MemberDAO;
+import blog.model.dto.MemberBean;
+
 public class MemberManagerment implements MemberManagermentIF {
-//
+	private static MemberManagerment instance = new MemberManagerment();
+	public static MemberManagerment getInstance(){
+		return instance;
+	}
+	
+	public void insertMember(MemberBean member) throws SQLException{
+		int result = 0;
+		try {
+			result = MemberDAO.insertMember(member);
+			if(result == 0){
+				throw new SQLException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+	
+	public MemberBean selectMemberByID(String id) throws SQLException ,RecordNotFoundException{
+		MemberBean member = null;
+		try {
+			member = MemberDAO.selectMemberByID(id);
+			if(member == null){
+				throw new RecordNotFoundException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+		return member;
+	}
+	
+	public JSONArray selectMemberList() throws SQLException ,RecordNotFoundException{
+		JSONArray list = null;
+		try {
+			list = MemberDAO.selectMemberList();
+			if(list == null){
+				throw new RecordNotFoundException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+		return list;
+	}
+	
+	public void updateMemberByID(String id) throws SQLException{
+		int result  = 0;
+		try {
+			result = MemberDAO.updateMemberByID(id);
+			if(result == 0){
+				throw new SQLException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+	
+	public void deleteMemberByID(String id) throws SQLException {
+		int result =0;
+		try {
+			result = MemberDAO.deleteMemberByID(id);
+			if(result ==0){
+				throw new SQLException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
 }
