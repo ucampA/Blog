7a6f3ca9f65aaef115a63ea3632b8af74033e771
JaesungP <ktyasha@gmail.com>
2014-10-18 14:41:00 +0900

    asd

--- BlogProject/src/blog/controller/blogController.java
+++ BlogProject/src/blog/controller/blogController.java
@@ -9,15 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 public class blogController extends HttpServlet {
     public blogController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		process(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		process(request, response);
+	}
+	
+	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
 	}
 
 }
