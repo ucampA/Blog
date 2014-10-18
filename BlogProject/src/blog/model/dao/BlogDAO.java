15511b1c49371d0267232147e5763b7550448723
SEULLEE <tmfdl311@gmail.com>
2014-10-18 14:03:40 +0900

    update blog.xml / blogDAO / dbutil

--- BlogProject/src/blog/model/dao/BlogDAO.java
+++ BlogProject/src/blog/model/dao/BlogDAO.java
@@ -1,6 +1,74 @@
 package blog.model.dao;
 
-public class BlogDAO {
+import java.sql.SQLException;
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
+import blog.model.dto.BlogBean;
+import blog.util.DBUtil;
+
+public class BlogDAO {
+	public static List<BlogBean> selectAllBranches() throws SQLException {
+		SqlSession session = DBUtil.getSqlSession();
+		try {
+			return session.selectList("Blog.selectAllPosts");
+		} finally {
+			session.close();
+		}
+	}
 	
+	public static BlogBean selectPostByNo(int no) throws SQLException {
+		SqlSession session = DBUtil.getSqlSession();
+		try {
+			return session.selectOne("Blog.selectPostByNo", no);
+		} finally {
+			session.close();
+		}
+	}
+	
+	public static int insertPost(BlogBean post) throws SQLException {
+		SqlSession session = DBUtil.getSqlSession(true);
+		try {
+			return session.insert("Blog.insertPost", post);
+		} finally {
+			session.close();
+		}
+	}
+	
+	public static int updatePost(BlogBean post) throws SQLException {
+		SqlSession session = DBUtil.getSqlSession(true);
+		try {
+			return session.update("Blog.updatePost", post);
+		} finally {
+			session.close();
+		}
+	}
+	
+	public static int updateReadCount() throws SQLException {
+		SqlSession session = DBUtil.getSqlSession(true);
+		try {
+			return session.update("Blog.updateReadCount");
+		} finally {
+			session.close();
+		}
+	}
+	
+	public static int deletePostByNo(int branchNo) throws SQLException {
+		SqlSession session = DBUtil.getSqlSession(true);
+		try {
+			return session.delete("Blog.deletePostByNo", branchNo);
+		} finally {
+			session.close();
+		}
+	}
+	
+	public static int deleteAllPosts() throws SQLException {
+		SqlSession session = DBUtil.getSqlSession(true);
+		try {
+			return session.delete("Blog.deleteAllPosts");
+		} finally {
+			session.close();
+		}
+	}
 }
