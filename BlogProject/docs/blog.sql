0473d1ef070f80f1890c56b66a026073e67d0c3a
SEULLEE <tmfdl311@gmail.com>
2014-10-18 12:59:07 +0900

    edit sql

--- BlogProject/docs/blog.sql
+++ BlogProject/docs/blog.sql
@@ -1,6 +1,23 @@
+DROP TABLE blog;
+DROP TABLE member CASCADE CONSTRAINT;
+
 CREATE TABLE member (
 	userid	varchar(20) primary key,
 	userpw	varchar(20) not null,
 	name	varchar(20) not null,
-	phone	varchar(13) not null
+	phone	varchar(13) not null,
+	blogname varchar(30) not null,
+	type char(1),
+	constraint member_type_ck check(type in ('A', 'U'))
 );
+
+CREATE TABLE blog (
+	no number(5) primary key,
+	title varchar(50) not null,
+	content varchar(500) not null,
+	wirteday date not null,
+	readcount number(4) not null,
+	userid	varchar(20),
+	constraint blog_userid_fk foreign key(userid) references member(userid)
+);
+
