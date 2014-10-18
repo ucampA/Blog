fa1e6adecd01053ce7f6371a4910dfd2ee12d858
hyo <hyo@Seob>
2014-10-18 17:17:12 +0900

    블로그 서비스 완성

--- BlogProject/src/blog/service/BlogManagerment.java
+++ BlogProject/src/blog/service/BlogManagerment.java
@@ -1,8 +1,10 @@
 package blog.service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import blog.exception.RecordNotFoundException;
+import blog.model.dao.BlogDAO;
 import blog.model.dto.BlogBean;
 
 public class BlogManagerment implements BlogManagermentIF{
@@ -11,8 +13,90 @@ public class BlogManagerment implements BlogManagermentIF{
 		return instance;
 	}
 	
-	public List<BlogBean> selectAllBranches() throws RecordNotFoundException{
+	public List<BlogBean> selectAllBranches() throws SQLException ,RecordNotFoundException{
 		List<BlogBean> list = null;
+		try {
+			list = BlogDAO.selectAllBranches();
+			if(list == null){
+				throw new RecordNotFoundException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
 		return list;
+	}
+	
+	public BlogBean selectPostByNo(int no) throws SQLException ,RecordNotFoundException{
+		BlogBean blog = null;
+		try {
+			blog = BlogDAO.selectPostByNo(no);
+			if(blog == null){
+				throw new RecordNotFoundException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+		return blog;
+	}
+	
+	public void insertPost(BlogBean post) throws SQLException{
+		int result = 0;
+		try {
+			result = BlogDAO.insertPost(post);
+			if(result == 0){
+				throw new SQLException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+	
+	public void updatePost(BlogBean post) throws SQLException{
+		int result = 0;
+		try {
+			result = BlogDAO.updatePost(post);
+			if(result == 0){
+				throw new SQLException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+	
+	public int updateReadCount() throws SQLException ,RecordNotFoundException{
+		int result = 0;
+		try {
+			result = BlogDAO.updateReadCount();
+			if(result == 0){
+				throw new RecordNotFoundException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+		return result;
+	}
+	
+	public void deletePostByNo(int branchNo) throws SQLException ,RecordNotFoundException{
+		int result = 0;
+		try {
+			result = BlogDAO.deletePostByNo(branchNo);
+			if(result == 0){
+				throw new RecordNotFoundException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+	
+	public void deleteAllPosts() throws SQLException ,RecordNotFoundException{
+		int result = 0;
+		try {
+			result = BlogDAO.deleteAllPosts();
+			if(result == 0){
+				throw new RecordNotFoundException();
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
 	}
 }
