d44e0514b161f9bfd0237efec4b57941153ed0a2
SEULLEE <tmfdl311@gmail.com>
2014-10-18 17:38:19 +0900

    dfg

--- BlogProject/src/blog/model/dao/MemberDAO.java
+++ BlogProject/src/blog/model/dao/MemberDAO.java
@@ -11,70 +11,70 @@ import org.apache.ibatis.session.SqlSession;
 import blog.model.dto.MemberBean;
 import blog.util.DBUtil;
 
+public class MemberDAO {
 
+	public static int insertMember(MemberBean member) throws SQLException {
+		SqlSession session = DBUtil.getSqlSession(true);
+		try {
+			return session.insert("Member.insertMember", member);
+		} finally {
+			session.close();
+		}
+	}
 
-public class MemberDAO{
-   
-   public static int insertMember(MemberBean member) throws SQLException {
-      SqlSession session = DBUtil.getSqlSession(true);
-      try {
-         return session.insert("Member.insertMember", member);
-      } finally {
-         session.close();
-      }
-   }
-   
-   public static MemberBean selectMemberByID(String id) throws SQLException {
-      SqlSession session = DBUtil.getSqlSession();
-      try {
-         return session.selectOne("Member.selectMemberByID", id);
-      } finally {
-         session.close();
-      }
-   }
-    
-   public static JSONArray selectMemberList() throws SQLException {
-      SqlSession session = DBUtil.getSqlSession(true);
-      JSONArray array = null;
-      try {
-         return JSONArray.fromObject(session.selectList("Member.selectMemberList"));
-      } finally {
-         session.close();
-      }
-   }
-   
-   public static int updateMemberByID(String id) throws SQLException {
-      SqlSession session = DBUtil.getSqlSession(true);
-      try {
-         return session.update("Member.updateMemberByID", id);
-      } finally {
-         session.close();
-      }
-   }
-   
-   public static int deleteMemberByID(String id) throws SQLException {
-      SqlSession session = DBUtil.getSqlSession(true);
-      try {
-         return session.update("Member.deleteMemberByID", id);
-      } finally {
-         session.close();
-      }
-   }
-   public static void main(String [] args){
-      JSONArray members = null;
-      try {
-         members = MemberDAO.selectMemberList();
-      } catch (SQLException e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-      }
-      Iterator allData = members.iterator();
-      //sdf
-      JSONObject jObj = null;
-      while(allData.hasNext()){
-         jObj = (JSONObject)allData.next();
-         System.out.println(jObj.get("userid"));
-      }
-   }
-   
+	public static MemberBean selectMemberByID(String id) throws SQLException {
+		SqlSession session = DBUtil.getSqlSession();
+		try {
+			return session.selectOne("Member.selectMemberByID", id);
+		} finally {
+			session.close();
+		}
+	}
+
+	public static JSONArray selectMemberList() throws SQLException {
+		SqlSession session = DBUtil.getSqlSession(true);
+		JSONArray array = null;
+		try {
+			return JSONArray.fromObject(session
+					.selectList("Member.selectMemberList"));
+		} finally {
+			session.close();
+		}
+	}
+
+	public static int updateMemberByID(String id) throws SQLException {
+		SqlSession session = DBUtil.getSqlSession(true);
+		try {
+			return session.update("Member.updateMemberByID", id);
+		} finally {
+			session.close();
+		}
+	}
+
+	public static int deleteMemberByID(String id) throws SQLException {
+		SqlSession session = DBUtil.getSqlSession(true);
+		try {
+			return session.update("Member.deleteMemberByID", id);
+		} finally {
+			session.close();
+		}
+	}
+
+	public static void main(String[] args) {
+		JSONArray members = null;
+		try {
+			members = MemberDAO.selectMemberList();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Iterator allData = members.iterator();
+		// sdf
+		JSONObject jObj = null;
+		while (allData.hasNext()) {
+			jObj = (JSONObject) allData.next();
+			System.out.println(jObj.get("userid"));
+		}
+	}
+
 }
